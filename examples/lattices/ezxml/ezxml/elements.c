1fa7af2c0a779a913a2cb904df5d77334c7a429e
John Talman <JohnDTalman@gmail.com>
2022-08-01 11:32:54 -0400

    $UAL/examples/lattices/ezxml/ezxml/

--- examples/lattices/ezxml/ezxml/elements.c
+++ examples/lattices/ezxml/ezxml/elements.c
@@ -9,21 +9,18 @@ void main(int argc,char*argv[]){
  ezxml_t mfield = ezxml_child(element, "mfield");
  
  for (element = ezxml_child(f1, "element"); element; element = element->next) {
+  type_attr = ezxml_attr(element, "type");
   name = ezxml_attr(element, "name");
-  if( !strcmp( argv[1], name)){
-   printf("HIT\n");
-   printf("%s:\n", name);
-   type_attr = ezxml_attr(element, "type");
-   printf("type %s:\n", type_attr);
-   l_attr = ezxml_attr(element, "l");
-   printf("l %s:\n", l_attr);
-   mfield = ezxml_child(element,"mfield");
-   a_attr = ezxml_attr(mfield, "a");
-   b_attr = ezxml_attr(mfield, "b");
-   printf("a %s:\n", a_attr);
-   printf("b %s:\n", b_attr);
-  }
-//element_child = ezxml_child(element, "type");
+  printf("%s ", type_attr);
+  l_attr = ezxml_attr(element, "l");
+//printf("%s = {.tag=\"%s\",.length=%s\n", name,name,l_attr);
+//printf("%s ", l_attr);
+  mfield = ezxml_child(element,"mfield");
+  a_attr = ezxml_attr(mfield, "a");
+  b_attr = ezxml_attr(mfield, "b");
+//printf("%s ", a_attr);
+//printf("%s \n", b_attr);
+  printf("%s = {.tag=\"%s\",.length=%s,.strength=%s,.%s_track=&%s_track};\n", name,name,l_attr,b_attr,type_attr,type_attr);
  }
  ezxml_free(f1); 
 }
