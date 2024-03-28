69785f93663a8e66daaa0af7e672fcba7c4b5d6c
John Talman <JohnDTalman@gmail.com>
2024-03-28 10:20:25 -0400

    $UAL/codes/E_m/

---
+++ codes/E_m/quad.cc
@@ -0,0 +1,46 @@
+#include <iomanip>
+#include <stdlib.h>
+
+#include "SMF/PacElemMultipole.h"
+#include "SMF/PacLattice.h"
+#include "ETEAPOT/Integrator/MltData.hh"
+#include "ETEAPOT/Integrator/BasicTracker.hh"
+#include "ETEAPOT2/Integrator/genMethods/Matrices.hh"
+
+#include "ETEAPOT2/Integrator/genMethods/Vectors.h"
+#include "ETEAPOT2/Integrator/genMethods/spinExtern"
+#include "ETEAPOT2/Integrator/genMethods/designExtern"
+#include "ETEAPOT2/Integrator/genMethods/bunchParticleExtern"
+
+namespace E_m{
+
+  class quad : public UAL::PropagatorNode {
+//class quad : public ETEAPOT::BasicTracker {
+
+  public:
+
+#include"quadMethods/classMethods"
+
+   inline ETEAPOT::MltData& getMltData();
+
+#include "quadMethods/propagate.method"
+#include "ETEAPOT2/Integrator/genMethods/get_vlcyMKS.method"
+#include "ETEAPOT2/Integrator/genMethods/passDrift.method"
+
+    /** Element length */
+    double m_l;
+
+    /** Complexity number */
+    double m_ir;
+
+    /** Mlt attributes */
+    ETEAPOT::MltData m_mdata;
+
+  };
+
+  inline ETEAPOT::MltData& quad::getMltData()
+  {
+      return m_mdata;
+  }
+
+}
