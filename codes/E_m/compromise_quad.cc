20451999d069f833ccbe6b8eb66810b4962af03b
John Talman <JohnDTalman@gmail.com>
2024-06-11 12:34:58 -0400

    $UAL/codes/E_m/...

--- codes/E_m/compromise_quad.cc
+++ codes/E_m/compromise_quad.cc
@@ -12,7 +12,6 @@
 #include"ETEAPOT/Integrator/DipoleData.hh"
 #include"ETEAPOT/Integrator/MltData.hh"
 
-//#include "ETEAPOT2/Integrator/BasicTracker.hh"
 #include"UAL/APF/PropagatorComponent.hh"
 
 #include"UAL/Common/Def.hh"
@@ -30,55 +29,50 @@
 namespace E_m{
 
   class compromise_quad : public UAL::PropagatorNode {
-//class quad : public ETEAPOT::BasicTracker {
 
   public:
-const char*  getType(){
-  return "JDT:::E_m::compromise_quad";
-}
+    const char*  getType(){
+     return "JDT:::E_m::compromise_quad";
+    }
 
 #include"ETEAPOT2/Integrator/bendMethods/classGlobals"
 #include"quadMethods/classMethods"
 
-   inline ETEAPOT::MltData& getMltData();
+    inline ETEAPOT::MltData& getMltData();
 
-#include "quadMethods/propagate.method"
-#include "ETEAPOT2/Integrator/genMethods/get_vlcyMKS.method"
-#include "ETEAPOT2/Integrator/genMethods/passDrift.method"
+#include"quadMethods/propagate.method"
+#include"ETEAPOT2/Integrator/genMethods/get_vlcyMKS.method"
+#include"ETEAPOT2/Integrator/genMethods/passDrift.method"
 
     /** Complexity number */
     double m_ir;
 
     double m_q;
 
-/*
-    double m_D_MinusTwo;      //   CW!
-    double m_D_MinusOne;      //   CW!
-    double m_D_PlusOne;       //   CW!
-    double m_D_PlusTwo;       //   CW!
+//                 symmetry: eXit = eNtry!
+    double m_NhD;             //   eNtry half Drift length;
+    double m_Nb;              //   eNtry body length, zero, 0, here (kick)
+    double m_NL;              //   Total eNtry Length: m_NL=m_NhD+m_Nb+m_NhD
 
-    double m_q_MinusOne;      //   CW!
-    double m_q_Zero;          //   CW!
-    double m_q_PlusOne;       //   CW!
+    double m_NQ;              //   eNtry strength;
+//                 symmetry: eXit = eNtry!
 
-    double m_l_MinusOne;      //   CW!
-    double m_l_Zero;          //   CW!
-    double m_l_PlusOne;       //   CW!
-*/
+    double m_MhD;             //   Middle half Drift length;
+    double m_Mb;              //   Middle length, zero, 0, here (kick)
+    double m_ML;              //   Total Middle Length: m_ML=m_MhD+m_Mb+m_MhD
 
-    double m_ehd;             //    entry half drift;
-    double m_mhd;             //   middle half drift;
+    double m_MQ;              //   Middle strength;
 
-    double m_el;              //    entry length;
-    double m_ml;              //   middle length;
+//                 symmetry: eXit = eNtry!
+    double m_XhD;             //   eXit half Drift length;
+    double m_Xb;              //   eXit body length, zero, 0, here (kick)
+    double m_XL;              //   Total eXit Length: m_XL=m_XhD+m_Xb+m_XhD
 
-    double m_eq;              //    entry strength;
-    double m_mq;              //   middle strength;
+    double m_XQ;              //   eXit strength;
+//                 symmetry: eXit = eNtry!
 
-    double m_L;               //   
+    double m_L;               //   Total compromise quad Length: m_L=m_NL+m_ML+m_XL
                               //  
-    //m_L=m_ehd+m_el+m_ehd+ m_mhd+m_ml+m_mhd+ m_ehd+m_el+m_ehd;
-
   };
 
 }
